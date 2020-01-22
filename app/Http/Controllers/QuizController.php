227eb07b8de2425c432bc63d9eaedbb013f320f9
Filip Mularski <mularskif@gmail.com>
2020-01-22 22:46:09 +0100

    check for sqlserver oracle postgresql

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -46,6 +46,107 @@ class QuizController extends Controller
         return view('mysql', ['ids' => $ids, 'texts' => $texts, 'paths' => $paths, 'correct' => $correct]);
     }
 
+    
+
+    public function sqlserver()
+    {
+        $sqlserver_tasks_texts_all = DB::select('SELECT Text FROM Tasks WHERE Category = ?', ['sqlserver']);
+        $sqlserver_tasks_img_paths_all = DB::select('SELECT ImgPath FROM Tasks WHERE Category = ?', ['sqlserver']);
+        $sqlserver_tasks_ids_all = DB::select('SELECT Id FROM Tasks WHERE Category = ?', ['sqlserver']);
+        $sqlserver_tasks_answers_all = DB::select('SELECT Answer FROM Tasks WHERE Category = ?', ['sqlserver']);
+
+        $indexes = array();
+
+        while ( count($indexes) < 5)
+        {
+            $index = rand(0, 9);
+            if ( in_array($index, $indexes))
+                continue;
+            array_push($indexes, $index);
+        }
+
+        $ids = array();
+        $texts = array();
+        $paths = array();
+        $correct = array();
+
+        for ( $i = 0; $i < count($indexes); $i++)
+        {
+            array_push($ids, $sqlserver_tasks_ids_all[$indexes[$i]]->Id);
+            array_push($texts, $sqlserver_tasks_texts_all[$indexes[$i]]->Text);
+            array_push($paths, $sqlserver_tasks_img_paths_all[$indexes[$i]]->ImgPath);
+            array_push($correct, $sqlserver_tasks_answers_all[$indexes[$i]]->Answer);
+        }
+
+        return view('sqlserver', ['ids' => $ids, 'texts' => $texts, 'paths' => $paths, 'correct' => $correct]);
+    }
+
+    public function oracle()
+    {
+        $oracle_tasks_texts_all = DB::select('SELECT Text FROM Tasks WHERE Category = ?', ['oracle']);
+        $oracle_tasks_img_paths_all = DB::select('SELECT ImgPath FROM Tasks WHERE Category = ?', ['oracle']);
+        $oracle_tasks_ids_all = DB::select('SELECT Id FROM Tasks WHERE Category = ?', ['oracle']);
+        $oracle_tasks_answers_all = DB::select('SELECT Answer FROM Tasks WHERE Category = ?', ['oracle']);
+
+        $indexes = array();
+
+        while ( count($indexes) < 5)
+        {
+            $index = rand(0, 9);
+            if ( in_array($index, $indexes))
+                continue;
+            array_push($indexes, $index);
+        }
+
+        $ids = array();
+        $texts = array();
+        $paths = array();
+        $correct = array();
+
+        for ( $i = 0; $i < count($indexes); $i++)
+        {
+            array_push($ids, $oracle_tasks_ids_all[$indexes[$i]]->Id);
+            array_push($texts, $oracle_tasks_texts_all[$indexes[$i]]->Text);
+            array_push($paths, $oracle_tasks_img_paths_all[$indexes[$i]]->ImgPath);
+            array_push($correct, $oracle_tasks_answers_all[$indexes[$i]]->Answer);
+        }
+
+        return view('oracle', ['ids' => $ids, 'texts' => $texts, 'paths' => $paths, 'correct' => $correct]);
+    }
+
+    public function postgresql()
+    {
+        $postgresql_tasks_texts_all = DB::select('SELECT Text FROM Tasks WHERE Category = ?', ['postgresql']);
+        $postgresql_tasks_img_paths_all = DB::select('SELECT ImgPath FROM Tasks WHERE Category = ?', ['postgresql']);
+        $postgresql_tasks_ids_all = DB::select('SELECT Id FROM Tasks WHERE Category = ?', ['postgresql']);
+        $postgresql_tasks_answers_all = DB::select('SELECT Answer FROM Tasks WHERE Category = ?', ['postgresql']);
+
+        $indexes = array();
+
+        while ( count($indexes) < 5)
+        {
+            $index = rand(0, 9);
+            if ( in_array($index, $indexes))
+                continue;
+            array_push($indexes, $index);
+        }
+
+        $ids = array();
+        $texts = array();
+        $paths = array();
+        $correct = array();
+
+        for ( $i = 0; $i < count($indexes); $i++)
+        {
+            array_push($ids, $postgresql_tasks_ids_all[$indexes[$i]]->Id);
+            array_push($texts, $postgresql_tasks_texts_all[$indexes[$i]]->Text);
+            array_push($paths, $postgresql_tasks_img_paths_all[$indexes[$i]]->ImgPath);
+            array_push($correct, $postgresql_tasks_answers_all[$indexes[$i]]->Answer);
+        }
+
+        return view('postgresql', ['ids' => $ids, 'texts' => $texts, 'paths' => $paths, 'correct' => $correct]);
+    }
+
     public function check(Request $request)
     {
         $answers = [
@@ -69,7 +170,8 @@ class QuizController extends Controller
 
         for ( $i = 0; $i < 5; $i++)
         {
-            if ( strcmp( strtolower(trim($answers[$i])), strtolower(trim($corrects[$i]))) == 0)
+            if ( strcmp( strtolower(str_replace(array(PHP_EOL, ' '), '', $answers[$i])), 
+                        strtolower(str_replace(array(PHP_EOL, ' '), '', $corrects[$i]))) == 0)
             {
                 $points++;
                 $correct_flags[$i] = 1;
@@ -78,87 +180,5 @@ class QuizController extends Controller
 
         return view('results', ['answers' => $answers, 'corrects' => $corrects, 'flags' => $correct_flags]);
     }
-
-    public function sqlserver()
-    {
-        $sqlserver_tasks_texts_all = DB::select('SELECT Text FROM Tasks WHERE Category = ?', ['sqlserver']);
-        $sqlserver_tasks_img_paths_all = DB::select('SELECT ImgPath FROM Tasks WHERE Category = ?', ['sqlserver']);
-
-        $indexes = array();
-
-        while ( count($indexes) < 5)
-        {
-            $index = rand(0, 9);
-            if ( in_array($index, $indexes))
-                continue;
-            array_push($indexes, $index);
-        }
-
-        $texts = array();
-        $paths = array();
-
-        for ( $i = 0; $i < count($indexes); $i++)
-        {
-            array_push($texts, $sqlserver_tasks_texts_all[$indexes[$i]]->Text);
-            array_push($paths, $sqlserver_tasks_img_paths_all[$indexes[$i]]->ImgPath);
-        }
-
-        return view('sqlserver', ['texts' => $texts, 'paths' => $paths]);
-    }
-
-    public function oracle()
-    {
-        $oracle_tasks_texts_all = DB::select('SELECT Text FROM Tasks WHERE Category = ?', ['oracle']);
-        $oracle_tasks_img_paths_all = DB::select('SELECT ImgPath FROM Tasks WHERE Category = ?', ['oracle']);
-
-        $indexes = array();
-
-        while ( count($indexes) < 5)
-        {
-            $index = rand(0, 9);
-            if ( in_array($index, $indexes))
-                continue;
-            array_push($indexes, $index);
-        }
-
-        $texts = array();
-        $paths = array();
-
-        for ( $i = 0; $i < count($indexes); $i++)
-        {
-            array_push($texts, $oracle_tasks_texts_all[$indexes[$i]]->Text);
-            array_push($paths, $oracle_tasks_img_paths_all[$indexes[$i]]->ImgPath);
-        }
-
-        return view('oracle', ['texts' => $texts, 'paths' => $paths]);
-    }
-
-    public function postgresql()
-    {
-        $postgresql_tasks_texts_all = DB::select('SELECT Text FROM Tasks WHERE Category = ?', ['postgresql']);
-        $postgresql_tasks_img_paths_all = DB::select('SELECT ImgPath FROM Tasks WHERE Category = ?', ['postgresql']);
-
-        $indexes = array();
-
-        while ( count($indexes) < 5)
-        {
-            $index = rand(0, 9);
-            if ( in_array($index, $indexes))
-                continue;
-            array_push($indexes, $index);
-        }
-
-        $texts = array();
-        $paths = array();
-
-        for ( $i = 0; $i < count($indexes); $i++)
-        {
-            array_push($texts, $postgresql_tasks_texts_all[$indexes[$i]]->Text);
-            array_push($paths, $postgresql_tasks_img_paths_all[$indexes[$i]]->ImgPath);
-        }
-
-        return view('postgresql', ['texts' => $texts, 'paths' => $paths]);
-    }
-
     
 }
